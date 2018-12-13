e592d6c99d74ac63ff8dc60d6dc628577db1f162
Jemirade Ayobami <45826284+heryorbhami@users.noreply.github.com>
2018-12-13 12:50:43 +0100

    Add files via upload

---
+++ day1_intro.py
@@ -0,0 +1,9 @@
+#Introduction
+#This simple code illustrate how to capture string and integer variables and also output them
+ 
+name =input("What is your name : ")
+print("Welcome " + name.title())
+occp = input("What is your Occupation : ")
+print("Your occupation is " + occp.title())
+age = int(input("Enter your age : "))
+print("Your age is ",age)
